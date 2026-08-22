b41ffb3b61c75ef908e1821ce85226ff2223b4d6
Jardielson <jardielsonbio@gmail.com>
2026-08-22 18:27:43 -0300

    feat: add a v2 for self-attention using nn.Linear from torch

--- src/self_attention__test.py
+++ src/self_attention__test.py
@@ -1,19 +1,25 @@
 import torch
-from self_attention import SelfAttention_v1
+from self_attention import SelfAttention_v1, SelfAttention_v2
 
 torch.manual_seed(123)
 
 inputs = torch.tensor(
-  [[0.43, 0.15, 0.89], # Your     (x^1)
-   [0.55, 0.87, 0.66], # journey  (x^2)
-   [0.57, 0.85, 0.64], # starts   (x^3)
-   [0.22, 0.58, 0.33], # with     (x^4)
-   [0.77, 0.25, 0.10], # one      (x^5)
-   [0.05, 0.80, 0.55]] # step     (x^6)
+    [
+        [0.43, 0.15, 0.89],  # Your     (x^1)
+        [0.55, 0.87, 0.66],  # journey  (x^2)
+        [0.57, 0.85, 0.64],  # starts   (x^3)
+        [0.22, 0.58, 0.33],  # with     (x^4)
+        [0.77, 0.25, 0.10],  # one      (x^5)
+        [0.05, 0.80, 0.55],
+    ]  # step     (x^6)
 )
 
 d_in = inputs.shape[1]
 d_out = 2
 
 sa_v1 = SelfAttention_v1(d_in, d_out)
-print(sa_v1(inputs))
+print("sa_v1: ", sa_v1(inputs))
+
+torch.manual_seed(789)
+sa_v2 = SelfAttention_v2(d_in, d_out)
+print("sa_v2: ", sa_v2(inputs))
