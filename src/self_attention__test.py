2f1a80c6b8e2376188378bf2f5e630611689e1a5
Jardielson <jardielsonbio@gmail.com>
2026-09-07 11:56:26 -0300

    feat: add causal attention mechanism

--- src/self_attention__test.py
+++ src/self_attention__test.py
@@ -1,5 +1,6 @@
 import torch
 from self_attention import SelfAttention_v1, SelfAttention_v2
+from causal_attention import CausalAttention
 
 torch.manual_seed(123)
 
@@ -18,8 +19,46 @@ d_in = inputs.shape[1]
 d_out = 2
 
 sa_v1 = SelfAttention_v1(d_in, d_out)
-print("sa_v1: ", sa_v1(inputs))
+# print("sa_v1: ", sa_v1(inputs))
 
 torch.manual_seed(789)
 sa_v2 = SelfAttention_v2(d_in, d_out)
-print("sa_v2: ", sa_v2(inputs))
+# print("sa_v2: ", sa_v2(inputs))
+
+
+queries = sa_v2.W_query(inputs)
+keys = sa_v2.W_key(inputs)
+attn_scores = queries @ keys.T
+attn_weights = torch.softmax(attn_scores / keys.shape[-1] ** 0.5, dim=-1)
+print(attn_weights)
+
+context_length = attn_scores.shape[0]
+mask_simple = torch.tril(torch.ones(context_length, context_length))
+print(mask_simple)
+
+masked_simple = attn_weights * mask_simple
+print(masked_simple)
+
+
+row_sums = masked_simple.sum(dim=-1, keepdim=True)
+masked_simple_norm = masked_simple / row_sums
+print(masked_simple_norm)
+
+
+mask = torch.triu(torch.ones(context_length, context_length), diagonal=1)
+masked = attn_scores.masked_fill(mask.bool(), -torch.inf)
+print(masked)
+
+attn_weights = torch.softmax(masked / keys.shape[-1] ** 0.5, dim=1)
+print(attn_weights)
+
+dropout = torch.nn.Dropout(0.5)
+torch.manual_seed(123)
+print("dropout(attn_weights): ", dropout(attn_weights))
+
+batch = torch.stack((inputs, inputs), dim=0)
+torch.manual_seed(123)
+context_length = batch.shape[1]
+ca = CausalAttention(d_in, d_out, context_length, 0.0)
+context_vecs = ca(batch)
+print("context.vecs.shape: ", context_vecs.shape)
