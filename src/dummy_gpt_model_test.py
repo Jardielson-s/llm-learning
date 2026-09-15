f6e09ddc2983d4c582a3e8cc0c36aca949f7310f
Jardielson <jardielsonbio@gmail.com>
2026-09-15 10:42:39 -0300

    feat: add gpt model

--- src/dummy_gpt_model_test.py
+++ src/dummy_gpt_model_test.py
@@ -2,7 +2,7 @@ import torch
 import tiktoken
 import torch.nn as nn
 
-from dummy_gpt_model import DummyGptModel, LayerNorm
+from dummy_gpt_model import DummyGptModel, LayerNorm, FeedForward
 from gpt_config import GPT_CONFIG_124M
 
 tokenizer = tiktoken.get_encoding("gpt2")
@@ -32,3 +32,8 @@ mean = out_ln.mean(dim=-1, keepdim=True)
 var = out_ln.var(dim=-1, unbiased=False, keepdim=True)
 print("Mean: \n", mean)
 print("Variance: \n", var)
+
+ffn = FeedForward(GPT_CONFIG_124M)
+x = torch.rand(2, 3, 768)
+out = ffn(x)
+print(out.shape)
